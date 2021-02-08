8b53e4d3ef42458338159e8334431fc035253280
Suraj Pandey <4copleyplace@gmail.com>
2021-02-08 01:52:10 -0500

    Question 4 Solution

---
+++ Question4_SurajPandey.php
@@ -0,0 +1,17 @@
+<?php
+function NumberOfDivisors($x): string
+{
+    $divisors = [];
+    for($i = 1; $i < $x; $i ++) {
+        if ($x % $i === 0) {
+            array_push($divisors, $i);
+        }
+    }
+    $numberOfDivisors = count($divisors);
+    $sumOfDivisors = array_sum($divisors);
+    return "The Number of Divisors is ".$numberOfDivisors.".\nThe Sum of Divisors is ".$sumOfDivisors. "\n";
+}
+
+$divisors = NumberOfDivisors ( 147 );
+print_r($divisors)
+?>
